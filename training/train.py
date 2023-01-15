cc556901b4ca05a7f9cd2351b9706ac059ce496b
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-01-14 21:49:39 -0500

    Created more training things

--- training/train.py
+++ training/train.py
@@ -8,8 +8,14 @@ ultralytics.checks() #Checks for successful install/import
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("training/datasets/FRC Object Detection-4/data.yaml")  # load the previous model here OR use a yolov8 model if you want to restart the transfer training ("yolov8s.pt")
 
-# Use the model
-training = model.train(data="training/datasets/FRC Object Detection-4/data.yaml", epochs=100)  #epochs can just be 25-30...
-training = model.val()  # evaluate model performance on the validation set
+def train():
+    model = YOLO("training/runs/detect/train/weights/best.pt")  # load the previous model here OR use a yolov8 model if you want to restart the transfer training ("yolov8s.pt")
+
+    # Use the model
+    training = model.train(data="training/datasets/FRC Object Detection-4/data.yaml", epochs=100)  #epochs can just be 25-30...
+    training = model.val()  # evaluate model performance on the validation set
+
+
+if __name__ == "__main__":
+    train()
