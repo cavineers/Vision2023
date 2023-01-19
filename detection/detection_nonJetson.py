863b32e513be9cc6a1ab695f418374f247be6446
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-01-19 01:16:44 -0500

    Added final (for now) training data

--- detection/detection_nonJetson.py
+++ detection/detection_nonJetson.py
@@ -6,11 +6,11 @@ from ultralytics import YOLO
 #If you are using a video
 
 #If you are inputting a yt video
-videoUrl = 'https://www.youtube.com/watch?v=s3r1axRoDyQ'
+videoUrl = 'https://youtu.be/fNOh7wYnriU'
 cap = cap_from_youtube(videoUrl)
 
 # Initialize YOLOv8 model
-model_path = "training/runs/detect/train6/weights/best.pt" #training/runs/detect/train/weights/best.pt
+model_path = "training/runs/detect/train7/weights/best.pt" #training/runs/detect/train/weights/best.pt
 model = YOLO(model_path)
 
 cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
