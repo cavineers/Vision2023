d0c0b504eb7bd20daa601cacd23b0f9f53226de2
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-01-29 21:47:32 -0500

    Feat: Initial Focus Determination Methods

--- detection/detect.py
+++ detection/detect.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from ultralytics import YOLO
 
@@ -6,7 +7,7 @@ from ultralytics import YOLO
 model_path = "training/runs/detect/train7/weights/best.pt" #training/runs/detect/train/weights/best.pt
 model = YOLO(model_path)
 
-results = model.predict(source=1, return_outputs=True) #Starts inferencing
+
 
 
 def getPoints(object):
@@ -18,38 +19,75 @@ def getPoints(object):
     x2 = object[2]
     y2 = object[3]
     return [x1, y1, x2, y2]
-def getDelta(points):
+def getDeltas(points):
     #Returns the delta (change) in x and y as an array
     deltX = (points[2] - points[0])
     deltY = (points[3] - points[1])
     return [deltX, deltY]
 
+def getCenter(deltas):
+    #Returns the center of the object
+    deltX = [deltas[0]]
+    deltY = [deltas[1]]
+
+    #Determine Midpoints
+
+    midX = (deltX[0] / 2)
+    midY = (deltY[0] / 2)
+    return [midX, midY] # Returns the midpoint/center of the object
+
 def getArea(deltas):
     area = (deltas[0] * deltas[1])
     return area
 
-def compareArea(largest, currentArea):
-    if currentArea > currentArea:
-        return currentArea
+def compareWeight(weight, f_weight):
+    if weight > f_weight:
+        return True
     else:
-        return largest
+        return False
 
-def determineFocus(object):
-    points = getPoints(object=object)
-    deltas = getDelta(points)
-    
+def determineFocus(object, f_weight): #Determines what object is in focus
+    '''
+        This function compares two objects based off a weight system
+        
+        Weights:
+        Area: x1
+        Position on Screen: x1
+        Distance: x2
+    '''
+    #Basic Current Object Info
+    points = getPoints(object)
+    deltas = getDeltas(points)
+    area = getArea(deltas)
+
+    #Determine Proximity to center of screen
+
+
+
+    #Pose Estimation
+    return True, 0
+    #Determine Weights
+
+
+
 
 
 
 
 def main():
+    
+    results = model(source=1, imgsz=640, return_outputs=True, conf=.60) #Starts inferencing
+
     while True:
-        focusedObj = None # Reset the focused obj every full iteration
+        focusedObj = None
+        focusedWeight = 0
         for resultDict in results:
             if resultDict:
-                focused = determineFocus(object)
-                if focused: # If the object is determined to be focused
-                    focusedObj = object 
+                for object in resultDict['det']:
+                    focused, weight = determineFocus(object, focusedWeight)
+                    if focused: # If the object is determined to be focused
+                        focusedObj = object
+                        focusedWeight = weight
             else: # No results -- Dict doesn't exist
                 print('No results')
 
