d7c2b0d4b3609fd4cfbb482e4233abeb740ff584
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-01-29 19:12:22 -0500

    Feat: getDelta, getArea

--- detection/detect.py
+++ detection/detect.py
@@ -6,23 +6,36 @@ from ultralytics import YOLO
 model_path = "training/runs/detect/train7/weights/best.pt" #training/runs/detect/train/weights/best.pt
 model = YOLO(model_path)
 
-results = model.predict(source=1, return_outputs=True)
+results = model.predict(source=1, return_outputs=True) #Starts inferencing
 
-for resultDict in results:
-    if resultDict:
-        #print(resultDict) Format {'det': array([[       1666(X1),         398(Y1),        1914(X2),         593(Y2),     0.78452(Conf),           1]], dtype=float32)}ß
-        for object in resultDict['det']: #object contains a list containing object data 
 
-            #Bottom Left Corner on a Non-mirrored input
-            x1 = object[0] #x val
-            y1 = object[1] #y val
+def getDelta(x1, y1, x2, y2):
+    #Returns the delta (change) in x and y as an array
+    deltX = (x2 - x1)
+    deltY = (y2 - y1)
+    return [deltX, deltY]
 
-            #Top Right Corner on a Non-mirrored input
-            x2 = object[2]
-            y2 = object[3]
+def getArea(deltaX, deltaY):
+    area = (deltaX * deltaY)
+    return area
 
-    else:
-        print('No results')
+def main():
+    for resultDict in results:
+        if resultDict:
+            #resultDict Format --> {'det': array([[       1666(X1),         398(Y1),        1914(X2),         593(Y2),     0.78452(Conf),           1]], dtype=float32)}ß
+            for object in resultDict['det']: #object contains a list containing object data 
+                #Top Left Corner on a Non-mirrored input
+                x1 = object[0] #x val
+                y1 = object[1] #y val
 
-print("^Done")
+                #Bottom Right Corner on a Non-mirrored input
+                x2 = object[2]
+                y2 = object[3]
+                #print(f'X1: {x1} Y1: {y1} \n X2: {x2} Y2: {y2}')
+
+        else: # No results -- Dict doesn't exist
+            print('No results')
+
+if __name__ == '__main__':
+    main()
 
