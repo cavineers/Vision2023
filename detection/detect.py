65dba866d23531b328eab58ae2843bd898ccd2ea
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-02-01 18:56:00 -0500

    Feat: Onnx inferencing

--- detection/detect.py
+++ detection/detect.py
@@ -3,23 +3,15 @@ import numpy as np
 import math
 import onnx
 import onnxruntime as ort
+import time
 
 from ultralytics import YOLO
+from utils import xywh2xyxy, nms, draw_detections
 
-
-ort_session = ort.InferenceSession("alexnet.onnx")
-
-outputs = ort_session.run(
-    None,
-    {"actual_input_1": np.random.randn(10, 3, 224, 224).astype(np.float32)},
-)
-print(outputs[0])
 
 # Initialize YOLOv8 model
-#net = cv2.dnn.readNetFromONNX('detection/finalweights/model.onnx')
-net = onnx.load('detection/finalweights/model.onnx')
 
-onnx.checker.check_model(net)
+
 
 
 
@@ -120,42 +112,165 @@ class Object():
             return False
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-'''
-Solving for 3D Measurements of an object
-'''
+
+
+class YOLOv8:
+
+    def __init__(self, path, conf_thres=0.7, iou_thres=0.5):
+        self.conf_threshold = conf_thres
+        self.iou_threshold = iou_thres
+
+        # Initialize model
+        self.initialize_model(path)
+
+    def __call__(self, image):
+        return self.detect_objects(image)
+
+    def initialize_model(self, path):
+        self.session = ort.InferenceSession(path)
+        # Get model info
+        self.get_input_details()
+        self.get_output_details()
+
+
+    def detect_objects(self, image):
+        input_tensor = self.prepare_input(image)
+
+        # Perform inference on the image
+        outputs = self.inference(input_tensor)
+
+        self.boxes, self.scores, self.class_ids = self.process_output(outputs)
+
+        return self.boxes, self.scores, self.class_ids
+
+    def prepare_input(self, image):
+        self.img_height, self.img_width = image.shape[:2]
+
+        input_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Resize input image
+        input_img = cv2.resize(input_img, (self.input_width, self.input_height))
+
+        # Scale input pixel values to 0 to 1
+        input_img = input_img / 255.0
+        input_img = input_img.transpose(2, 0, 1)
+        input_tensor = input_img[np.newaxis, :, :, :].astype(np.float32)
+
+        return input_tensor
+
+
+    def inference(self, input_tensor):
+        start = time.perf_counter()
+        outputs = self.session.run(self.output_names, {self.input_names[0]: input_tensor})
+
+        # print(f"Inference time: {(time.perf_counter() - start)*1000:.2f} ms")
+        return outputs
+
+    def process_output(self, output):
+        predictions = np.squeeze(output[0]).T
+
+        # Filter out object confidence scores below threshold
+        scores = np.max(predictions[:, 4:], axis=1)
+        predictions = predictions[scores > self.conf_threshold, :]
+        scores = scores[scores > self.conf_threshold]
+
+        if len(scores) == 0:
+            return [], [], []
+
+        # Get the class with the highest confidence
+        class_ids = np.argmax(predictions[:, 4:], axis=1)
+
+        # Get bounding boxes for each object
+        boxes = self.extract_boxes(predictions)
+
+        # Apply non-maxima suppression to suppress weak, overlapping bounding boxes
+        indices = nms(boxes, scores, self.iou_threshold)
+
+        return boxes[indices], scores[indices], class_ids[indices]
+
+    def extract_boxes(self, predictions):
+        # Extract boxes from predictions
+        boxes = predictions[:, :4]
+
+        # Scale boxes to original image dimensions
+        boxes = self.rescale_boxes(boxes)
+
+        # Convert boxes to xyxy format
+        boxes = xywh2xyxy(boxes)
+
+        return boxes
+
+    def rescale_boxes(self, boxes):
+
+        # Rescale boxes to original image dimensions
+        input_shape = np.array([self.input_width, self.input_height, self.input_width, self.input_height])
+        boxes = np.divide(boxes, input_shape, dtype=np.float32)
+        boxes *= np.array([self.img_width, self.img_height, self.img_width, self.img_height])
+        return boxes
+
+    def draw_detections(self, image, draw_scores=True, mask_alpha=0.4):
+
+        return draw_detections(image, self.boxes, self.scores,
+                               self.class_ids, mask_alpha)
+
+    def get_input_details(self):
+        model_inputs = self.session.get_inputs()
+        self.input_names = [model_inputs[i].name for i in range(len(model_inputs))]
+
+        self.input_shape = model_inputs[0].shape
+        self.input_height = self.input_shape[2]
+        self.input_width = self.input_shape[3]
+
+    def get_output_details(self):
+        model_outputs = self.session.get_outputs()
+        self.output_names = [model_outputs[i].name for i in range(len(model_outputs))]
 
 
 
 
-
-
-
-#def main():
-    
-    # results = model(source=0, imgsz=640, return_outputs=True, conf=.60) #Starts inferencing
-    # while True:
-    #     print("Initilizing")
-    #     for dict in results:
-    #         focusedObj = None # Placeholder for focused object
-    #         if not dict: # If no results, continue
-    #             print("No Results") 
-    #             continue 
-    #         for object in dict['det']:
-    #             m_object = Object(object)
-    #             if not focusedObj: #See if there is a focused object if not set it to the first object
-    #                 focusedObj = m_object
-    #             elif m_object.compareWeight(focusedObj.weight):
-    #                 focusedObj = m_object
-    #             else:
-    #                 continue
-    #             angle = angleSolver(m_object)
-    #             print(angle)
-    #             if m_object.compareWeight(focusedObj.weight):
-    #                 focusedObj = m_object
-    #         #print("Focused Object: ", focusedObj.classID)
+class imageHandler():
+    def __init__(self, img):
+        self.img = img
+        self.imgHeight = img.shape[0]
+        self.imgWidth = img.shape[1]
+        self.processedImg = self.preProcess()
         
-                    
+    def preProcess(self):
+        self.processedImg = cv2.resize(self.img, (640, 640))
+        
+class cameraHandler():
+    def __init__(self, src):
+        self.src = src
+        self.cap = cv2.VideoCapture(self.src)
+    
+    def getFrame(self):
+        ret, frame = self.cap.read()
+        return frame
 
-# if __name__ == '__main__':
-#     main()
+
+
+
+if __name__ == '__main__':
+    cam = cameraHandler(1)
+    while True:
+        model_path = "detection/finalweights/model.onnx"
+
+        # Initialize YOLOv8 object detector
+        yolov8Detector = YOLOv8(model_path, conf_thres=0.6, iou_thres=0.5)
+
+        img = cam.getFrame()
+        if type(img) == type(None):
+            continue
+        # Detect Objects
+        yolov8Detector(img)
+
+        # Draw detections
+        combined_img = yolov8Detector.draw_detections(img)
+        cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
+        cv2.imshow("Output", combined_img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
 
--- training/export.py
+++ training/export.py
@@ -4,4 +4,5 @@ model = torch.load('training/runs/detect/train7/weights/best.pt', map_location=t
 
 dummy_input = torch.randn(1, 3, 640, 640)  # (batch_size, channels, height, width)
 # # Export to ONNX
-torch.onnx.export(model, dummy_input, 'model.onnx', verbose=True, opset_version=11, input_names=['images'], output_names=['output0'])
+torch.onnx.export(model, dummy_input, 'model.onnx', verbose=True, opset_version=11, input_names=['images'], output_names=['output0'],  dynamic_axes={'modelInput' : {0 : 'batch_size'},    # variable length axes 
+                                'modelOutput' : {0 : 'batch_size'}})
