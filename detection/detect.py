5efa5c71619a6df4a1420bee6e1fcb29d3c3da3d
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-02-02 10:48:26 -0500

    Updated get angle func

--- detection/detect.py
+++ detection/detect.py
@@ -15,8 +15,8 @@ class angleSolver():
         self.object = object.object
         self.classID = object.classID
 
-        self.horizontalFOV = 0
-        self.screenWidth = 640
+        self.horizontalFOV = 70.4
+        self.screenWidth = 384
 
         self.centerOfBox = object.getCenter()
         self.centerOfScreen = self.screenWidth/2
@@ -24,11 +24,13 @@ class angleSolver():
         self.angleToObj = self.solveAngle()
     
     def getMagToObject(self): #Gets the straight line object center of screen -> center of box
-        return math.dist(self.centerOfBox[0], self.centerOfScreen)
+        mag = math.fabs(self.centerOfScreen - self.centerOfBox[0])
+        return mag
     
     def solveAngle(self):
         degreesPerPixel = self.horizontalFOV / self.screenWidth
         distanceFromCenter = self.getMagToObject() # Distance from center of screen to center of box in pixels
+        print(distanceFromCenter)
         angle = distanceFromCenter * degreesPerPixel # Get Angle by Multiplying number of pixels by degrees per pixel
         return angle
     
@@ -49,14 +51,14 @@ class Object():
         self.points = self.getPoints()
         self.deltas = self.getDeltas(self.points)
         self.area = self.getArea(self.deltas)
-        self.center = self.getCenter(self.deltas)
+        self.center = self.getCenter()
         self.proximity = self.getProximityCenter(self.center)
 
         self.weight = self.calculateWeight()
 
     def getPoints(self):
         #Top Left Corner on a Non-mirrored input
-        print(object)
+
         x1 = self.object[0] #x val
         y1 = self.object[1] #y val
 
@@ -71,10 +73,10 @@ class Object():
         deltY = (points[3] - points[1])
         return [deltX, deltY]
 
-    def getCenter(self, deltas):
+    def getCenter(self):
         #Returns the center of the object
-        deltX = [deltas[0]]
-        deltY = [deltas[1]]
+        deltX = [self.deltas[0]]
+        deltY = [self.deltas[1]]
 
         #Determine Midpoints
 
@@ -95,7 +97,7 @@ class Object():
         return z
 
     def calculateWeight(self): #Calculates weights
-        weight = (self.area / 0.001) + (self.proximity * 1)
+        weight = (math.sqrt(self.area)) + (self.proximity * 1)
         #print(f'{(self.area * 0.001)} + {(self.proximity * 1)} + {(self.distance * 2)}')
         return weight
     
@@ -134,10 +136,9 @@ def main():
                     focusedObj = m_object
                 else:
                     continue
-                angle = angleSolver(m_object)
-                print(angle)
-                if m_object.compareWeight(focusedObj.weight):
-                    focusedObj = m_object
+                angleSolved = angleSolver(focusedObj)
+                print(f'Angle to object {angleSolved.angleToObj}')
+                    
             #print("Focused Object: ", focusedObj.classID)
         
                     
