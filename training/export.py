235b743e296d153b315c792159d286f9995629e2
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-02-13 21:48:23 -0500

    New Model

--- training/export.py
+++ training/export.py
@@ -1,6 +1,6 @@
 import torch
 
-model = torch.load('training/runs/detect/train7/weights/nanoModel.pt', map_location=torch.device('cpu'))['model'].float().fuse().eval()  # load FP32 model
+model = torch.load('/home/sam/Vision2023/training/runs/detect/train11/weights/best.pt')['model'].int().fuse().eval()  # load FP32 model
 
 dummy_input = torch.randn(1, 3, 640, 640)  # (batch_size, channels, height, width)
 # # Export to ONNX
